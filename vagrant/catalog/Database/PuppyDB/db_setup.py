31e169ffbd45c45d99132e42bc89fce4865b5b0d
hjerpe <hjerpe.adam@gmail.com>
2016-05-08 16:49:31 +0200

    trying python sqlalchemy api

---
+++ vagrant/catalog/Database/PuppyDB/db_setup.py
@@ -0,0 +1,35 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+
+class Shelter(Base):
+	__tablename__ = 'shelter'
+	name = Column(String(80), nullable=False)
+	address = Column(String(80), nullable=False)
+	city = Column(String(80), nullable=False)
+	state = Column(String(80), nullable=False)
+	zipCode = Column(String(5), nullable=False)
+	website = Column(String(80), nullable=True)
+	id = Column(Integer, primary_key=True)
+
+
+class Puppy(Base):
+	__tablename__ = 'puppy'
+	
+	name = Column(String(80), nullable=False)
+	dateOfBirth = Column(Date, nullable=True)
+	gender = Column(String(6), nullable=False)
+	weight = Column(Integer, nullable=False)
+	shelter_id = Column(Integer, ForeignKey('shelter.id'))
+	id = Column(Integer, primary_key=True)
+
+	shelter = relationship(Shelter)
+
+
+engine = create_engine('sqlite:///puppyshelter.db')
+Base.metadata.create_all(engine)
